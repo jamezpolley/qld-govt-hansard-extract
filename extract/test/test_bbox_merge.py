9cd2a642edf97479ae61a344bdcb198cedb32b97
Kiz <kieran.mcdougall@aamcommercial.com.au>
2019-07-07 10:28:21 +1000

    Fixed up a test

--- extract/test/test_bbox_merge.py
+++ extract/test/test_bbox_merge.py
@@ -7,18 +7,9 @@ import unittest
 class TestBBoxMergeMergesCoordinatesCorrectly(unittest.TestCase):
     def setUp(self):
         bboxes = [
-            BBox(Coordinate(466.681, 788.160), Coordinate(472.219, 801.755)),
-            BBox(Coordinate(472.199, 788.160), Coordinate(477.736, 801.755)),
-            BBox(Coordinate(477.717, 788.160), Coordinate(480.485, 801.755)),
-            BBox(Coordinate(480.475, 788.160), Coordinate(488.772, 801.755)),
-            BBox(Coordinate(488.876, 788.160), Coordinate(494.413, 801.755)),
-            BBox(Coordinate(494.516, 788.160), Coordinate(499.496, 801.755)),
-            BBox(Coordinate(499.316, 788.160), Coordinate(502.085, 801.755)),
-            BBox(Coordinate(502.195, 788.160), Coordinate(507.733, 801.755)),
-            BBox(Coordinate(507.713, 788.160), Coordinate(513.251, 801.755)),
-            BBox(Coordinate(513.231, 788.160), Coordinate(518.769, 801.755)),
-            BBox(Coordinate(518.870, 788.160), Coordinate(524.408, 801.755)),
-            BBox(Coordinate(524.519, 788.160), Coordinate(527.287, 801.755)),
+            BBox(Coordinate(466.681, 770.160), Coordinate(472.219, 801.755)),
+            BBox(Coordinate(472.199, 771.160), Coordinate(477.736, 100.755)),
+            BBox(Coordinate(524.519, 788.160), Coordinate(527.287, 70.755)),
         ]
         self.merged_bbox = bbox_merge(bboxes)
 
@@ -32,4 +23,4 @@ class TestBBoxMergeMergesCoordinatesCorrectly(unittest.TestCase):
         self.assertEqual(self.merged_bbox.lower_right_coordinate.x, 527.287)
 
     def test_lower_right_y(self):
-        self.assertEqual(self.merged_bbox.lower_right_coordinate.y, 801.755)
+        self.assertEqual(self.merged_bbox.lower_right_coordinate.y, 70.755)
